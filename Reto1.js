e95c0c9fe1b5d2db3957179cf1bd96f8d416951b
Silent-Storm04 <47425042+Silent-Storm04@users.noreply.github.com>
2019-02-07 09:46:58 -0600

    Create Reto1.js

---
+++ Reto1.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+	fill(0,0,0);
+	stroke(300,300,300);
+	line(200,220,0,400);
+	line(200,220,400,400)
+	line(200,220,400,-16000)
+}
