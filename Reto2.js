9b3259c214a55c4a93201c69da18a23ed4c8b7ea
Silent-Storm04 <47425042+Silent-Storm04@users.noreply.github.com>
2019-02-07 09:47:24 -0600

    Create Reto2.js

---
+++ Reto2.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0,0,0);
+	fill(0,0,0);
+	stroke(300,300,300);
+	fill(400,0,0)
+	rect(0,0,140,399);
+	fill(0,400,0);
+	rect(141,0,140,399);
+	fill(0,0,400)
+	rect(282,0,117,399)
+}
